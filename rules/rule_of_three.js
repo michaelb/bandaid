4825066355711acedd8b1c1183cf5a4acdec2ac6
Michael Bleuez <michael.bleuez2@gmail.com>
2025-07-15 22:42:41 +0200

    fix: repair rule of three rule

--- rules/rule_of_three.js
+++ rules/rule_of_three.js
@@ -8,23 +8,25 @@ const ro3 = {
 
     let count = 0;
     for (let sentence of sentences) {
+      console.log("bandaid " + sentence)
       let parts = sentence.split(",");
       let nb_of_commas = parts.length - 1;
       if (nb_of_commas < 2) {
         continue;
       }
-      let has_and_or = (parts[1].includes("and") || parts[1].includes("or"));
-      let has_and_or_not_just_after_comma = !(sentence.includes(", and") || sentence.includes(", or"));
+      let has_and_or = (parts[1].includes("and ") || parts[1].includes("or "));
+      let has_and_or2 = (parts[2].includes("and ") || parts[2].includes("or "));
+      let has_and_or_not_just_after_comma = !(sentence.includes(", and ") || sentence.includes(", or "));
 
-      if (nb_of_commas == 1 && has_and_or && has_and_or_not_just_after_comma) {
+      if ((nb_of_commas == 1 && has_and_or && has_and_or_not_just_after_comma) || (nb_of_commas == 2 && has_and_or2 && !has_and_or_not_just_after_comma)) {
         count += 1;
       }
     }
 
-    console.log("bandaid ro3: count " + count + "   text length: ", text.length);
-    // 1 occurence every 1000 chars should count for 
+    console.log("bandaid ro3: count " + count + "   text length: " + text.length);
+    // 1 occurence every 400 chars should count for 
     // 10% suspiciousness, but max 50%
-    return Math.min(count * 1000 / text.length * 10, 50);
+    return Math.min(count * 400 / text.length * 10, 50);
   }
 
 }
